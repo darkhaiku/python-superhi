53694f8ac00dd5a6fa089583e37ed09017f13d70
darkhaiku <pooya.gh.alamdari@hotmail.com>
2022-10-29 12:19:49 +0330

    12 col and 10 row generate

--- generate_land.py
+++ generate_land.py
@@ -7,12 +7,13 @@ data = ['a', 'b', 'c', 'd', 'e', 'f']
 
 print(f'Generate a landscape which is {cols} by {rows}')
 
-row_string = ''
+for row in range(rows):
+    row_string = ''
 
-for col in range(cols):
-    r = random.choice(data)
-    row_string = row_string + r
+    for col in range(cols):
+        r = random.choice(data)
+        row_string += r
 
-print(row_string)
+    print(row_string)
 
 print('finished generating landscape')
